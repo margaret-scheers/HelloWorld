e7cdf5b64c22e4ed24a225ec256ed523dfe07bf3
margaret-scheers <margaret.scheers@pobox.com>
2021-03-18 13:29:59 -0500

    Implement validation for post

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -12,7 +14,11 @@ import java.util.Date;
 public class User {
 
     private Integer id;
+
+    @Size(min=2, message="Name should have at least 2 characters")
     private String name;
+
+    @Past
     private Date birthdate;
 
     @Override
